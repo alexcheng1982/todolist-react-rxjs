78a6ef175b26a7c5fb8c2c11517a318358d07e52
Fu Cheng <alexcheng1982@gmail.com>
2016-07-29 22:59:49 +1200

    cleanup unused code

--- app/containers/TodoApp.tsx
+++ app/containers/TodoApp.tsx
@@ -1,7 +1,6 @@
 
 import * as React from 'react';
 import AppBar from 'material-ui/AppBar';
-import RaisedButton from 'material-ui/RaisedButton';
 import Todo from '../components/Todo';
 
 export default class TodoApp extends React.Component<any, any> {
--- app/service/todos.ts
+++
@@ -1,6 +0,0 @@
-import { ITodo, ITodoList } from '../model/todo';
-import { Observable } from 'rxjs/Rx';
-
-export interface TodosService {
-  list(): Observable<ITodo>;
-}
